0e5e69378b82201b1a271bbb8b9d1b99fa331b1a
Hrubak <hrubak@gmail.com>
2014-12-01 13:10:41 -0500

    Update apply.sh

--- apply.sh
+++ apply.sh
@@ -59,8 +59,8 @@ set -e
 #http_patch https://dl.dropboxusercontent.com/u/13144052/CM11/hrubak/0001-SIGNAL-STRENGTH-MOD.patch
 #http_patch https://dl.dropboxusercontent.com/u/13144052/CM11/hrubak/0001-Make-the-signalbars-show-when-not-connected-or-conne.patch
 #cdb
-#79187 
-repopick -b 79965 79749 79764 79857 77856
+# 
+repopick -b 79965 79749 79764 79857 77856 79187
 
 ##### SUCCESS ####
 SUCCESS=true
