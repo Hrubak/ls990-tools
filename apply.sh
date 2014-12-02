9c52b79e15e36c45791cc19fb26d4bd537e9c5d3
Hrubak <hrubak@gmail.com>
2014-12-02 13:06:59 -0500

    Update apply.sh

--- apply.sh
+++ apply.sh
@@ -52,15 +52,19 @@ set -e
 
 ################ Apply Patches Below ####################
 
-#repo start auto frameworks/base
-#echo "Add signal strength mod"
-#cdv frameworks/base
-#git reset --hard
-#http_patch https://dl.dropboxusercontent.com/u/13144052/CM11/hrubak/0001-SIGNAL-STRENGTH-MOD.patch
-#http_patch https://dl.dropboxusercontent.com/u/13144052/CM11/hrubak/0001-Make-the-signalbars-show-when-not-connected-or-conne.patch
-#cdb
-#   
-repopick -b 79965 80156 79749 79764 79857 77856 79135 78864 80056 79187 79497
+#80156  ls990: add liblgderp and missing bt firmware
+#79965  ls990: bring up overlays
+#79749  DCTracker: HACK Fix eHRPD/LTE data connection
+#79764  Telephony: DcTracker: Fix CDMA APN Data issues.
+#79857  LgeLteRIL: Lollipop fixups for CDMA
+#77856  Telephony: Allow ruim to fetch in CDMA LTE mode
+#79135  g3-common: Change LCD density to 560dpi
+#78864  sepolicy: add device specific-sepolicies
+#80056  init: cleanup and juggling act
+#79187  UiccController: Query GET_SIM_STATUS when radio state is ON
+#79497  g3: selinux enforcing <--NOT YET!!!!
+
+repopick -b 80156 79965 79749 79764 79857 77856 79135 78864 80056 79187 #79497
 
 ##### SUCCESS ####
 SUCCESS=true
