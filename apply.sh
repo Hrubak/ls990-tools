1fd11973dd84db39b3121ee2b66a16e64117f401
Hrubak <hrubak@gmail.com>
2014-12-03 15:58:36 -0500

    try without ril patches

--- apply.sh
+++ apply.sh
@@ -53,24 +53,22 @@ set -e
 ################ Apply Patches Below ####################
 
 #79965  ls990: bring up overlays
-#79749  DCTracker: HACK Fix eHRPD/LTE data connection
-#79764  Telephony: DcTracker: Fix CDMA APN Data issues.
-#79857  LgeLteRIL: Lollipop fixups for CDMA
-#77856  Telephony: Allow ruim to fetch in CDMA LTE mode
 #79135  g3-common: Change LCD density to 560dpi
 #78864  sepolicy: add device specific-sepolicies
 #80056  init: cleanup and juggling act
-#79187  UiccController: Query GET_SIM_STATUS when radio state is ON
-#80167  media_profiles: audio and video bitrate must match on matching sizes
-#80168  props: disable hw aac encoder
 #79920  keylayout: remove deprecated wake flags
 #79630  media_profiles: fix 4k recording profile
 #79497  g3: selinux enforcing <--NOT YET!!!!
 #80229  ls990: select correct nfc config
 #80240  ls990: use pn544 as NFC chipset and include lib
 
+#79749  DCTracker: HACK Fix eHRPD/LTE data connection
+#79764  Telephony: DcTracker: Fix CDMA APN Data issues.
+#79857  LgeLteRIL: Lollipop fixups for CDMA
+#77856  Telephony: Allow ruim to fetch in CDMA LTE mode
+#79187  UiccController: Query GET_SIM_STATUS when radio state is ON
 
-repopick -b 79965 79749 79764 79857 77856 79135 78864 80056 79187 80167 80168 79920 79630 80229 80240
+repopick -b 79965 79135 78864 80056 79920 79630 80229 80240 #79749 79764 79857 77856 79187
 
 ##### SUCCESS ####
 SUCCESS=true
