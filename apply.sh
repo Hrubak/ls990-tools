667364206e6c8f8b0e08a9048a0a228641bcd481
Hrubak <hrubak@gmail.com>
2014-12-09 08:43:03 -0500

    Update apply.sh

--- apply.sh
+++ apply.sh
@@ -55,7 +55,6 @@ set -e
 #79965  ls990: bring up overlays
 #79135  g3-common: Change LCD density to 560dpi
 #78864  sepolicy: add device specific-sepolicies
-#80056  init: cleanup and juggling act
 #79920  keylayout: remove deprecated wake flags
 #79630  media_profiles: fix 4k recording profile
 #79497  g3: selinux enforcing <--NOT YET!!!!
@@ -64,14 +63,13 @@ set -e
 #Ril Fixes
 #79749  DCTracker: HACK Fix eHRPD/LTE data connection
 #79764  Telephony: DcTracker: Fix CDMA APN Data issues.
-#79857  LgeLteRIL: Lollipop fixups for CDMA
 #77856  Telephony: Allow ruim to fetch in CDMA LTE mode
 #NFC fixes
 #80229  ls990: select correct nfc config
 #80240  ls990: use pn544 as NFC chipset and include lib
 #80338  ls990: Build the default pn54x HAL
 
-repopick -b 79965 79135 78864 80056 79920 79630 79749 79764 79857 77856 81089 #80229 80240 80338
+repopick -b 79965 79135 78864 79920 79630 79749 79764 77856 81089 #80229 80240 80338
 
 
 ##### SUCCESS ####
