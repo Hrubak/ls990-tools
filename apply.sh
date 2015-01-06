5c54733552eda451fd450ec1ced4da529140ce60
Hrubak <hrubak@gmail.com>
2015-01-06 13:43:10 -0500

    Update apply.sh

--- apply.sh
+++ apply.sh
@@ -67,7 +67,7 @@ set -e
 #80229  ls990: select correct nfc config
 #80240  ls990: use pn544 as NFC chipset and include lib
 
-repopick -b 79630 81089 81582 82710 83211 83210 80229 80240 84143 #82679
+repopick -b 79630 81582 82710 83211 83210 80229 80240 84143 #82679
 
 ##### SUCCESS ####
 SUCCESS=true
